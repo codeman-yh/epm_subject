2ee0f01f5cc83dc5e8fb84f6dd87c324538a1e89
codeman <270205047@qq.com>
2023-04-22 23:01:29 +0800

    test pretter

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn RReact
+          Learn React
         </a>
       </header>
     </div>
